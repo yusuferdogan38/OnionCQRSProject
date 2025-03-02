0b312b2734449464bdd8269b57dffcc5ef007116
Yusuf Erdoğan <136724876+yusuferdogan38@users.noreply.github.com>
2025-03-03 02:42:09 +0300

    Added UnitOfWork

---
+++ Core/CleanCqrs.Application/UnitOfWorks/IUnitOfwork.cs
@@ -0,0 +1,20 @@
+﻿using CleanCqrs.Application.Repositories;
+using CleanCqrs.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCqrs.Application.UnitOfWorks
+{
+    public interface IUnitOfwork:IAsyncDisposable
+    {
+        IReadRepository<T> GetReadRepository<T>() where T : class, IEntityBase, new();
+        IWriteRepository<T> GetWriteRepository<T>() where T : class, IEntityBase, new();
+        
+        Task<int> SaveAsync();
+
+        int Save();
+    }
+}
---
+++ Infrastructure/CleanCqrs.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using CleanCqrs.Application.Repositories;
+using CleanCqrs.Application.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanCqrs.Persistence.Context;
+
+using CleanCqrs.Persistence.Repositories;
+
+namespace CleanCqrs.Persistence.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfwork
+    {
+        private readonly AppDbContext dbContext;
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async ValueTask DisposeAsync() => await dbContext.DisposeAsync();
+        public int Save() => dbContext.SaveChanges();
+        public Task<int> SaveAsync() => dbContext.SaveChangesAsync();
+        IReadRepository<T> IUnitOfwork.GetReadRepository<T>() => new ReadRepository<T>(dbContext);
+        IWriteRepository<T> IUnitOfwork.GetWriteRepository<T>() => new WriteRepository<T>(dbContext);
+
+    }
+}
