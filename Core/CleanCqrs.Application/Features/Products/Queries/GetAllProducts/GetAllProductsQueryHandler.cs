60a327b7f2244784fb8b71d4cba6bc053ebc1dee
Yusuf Erdoğan <136724876+yusuferdogan38@users.noreply.github.com>
2025-03-04 02:57:51 +0300

    Added MediatR  and Cqrs for entity

---
+++ Core/CleanCqrs.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using CleanCqrs.Application.UnitOfWorks;
+using CleanCqrs.Domain.Entityies;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCqrs.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQueryRequest, IList<GetAllProductsQueryResponse>>
+    {
+        private readonly IUnitOfwork unitOfWork;
+        public GetAllProductsQueryHandler(IUnitOfwork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public async Task<IList<GetAllProductsQueryResponse>> Handle(GetAllProductsQueryRequest request, CancellationToken cancellationToken)
+        {
+            
+            var products= await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+            List<GetAllProductsQueryResponse> response = new();
+            foreach (var product in products)
+            {
+                response.Add(new GetAllProductsQueryResponse
+                {
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price-(product.Price*product.Discount/100),
+                    Discount = product.Discount
+                });
+            }
+
+
+            return response;
+        }
+    }
+}
--- Core/CleanCqrs.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryResponse.cs
+++ Core/CleanCqrs.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryResponse.cs
@@ -1,15 +1,13 @@
-﻿using CleanCqrs.Domain.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CleanCqrs.Domain.Entityies
+namespace CleanCqrs.Application.Features.Products.Queries.GetAllProducts
 {
-    public class Product:EntityBase
+    public class GetAllProductsQueryResponse
     {
-
 
         public string Name { get; set; }
 
@@ -18,14 +16,5 @@ namespace CleanCqrs.Domain.Entityies
         public decimal Price { get; set; }
 
         public decimal Discount { get; set; }
-
-        public int BrandId { get; set; }
-     
-
-        public Brand Brand { get; set; }
-
-        ICollection<Category> Categories { get; set; }
-
-
     }
 }
